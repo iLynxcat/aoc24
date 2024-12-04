a98bdf4f7d73890374494870d62f6f04022415a9
Lynx <141365347+iLynxcat@users.noreply.github.com>
2024-12-04 07:31:18 -0600

    Add day 1 code & tests woah did I just do TDD??

---
+++ days/1.test.ts
@@ -0,0 +1,45 @@
+import { describe, test, beforeAll, beforeEach, expect } from "bun:test";
+import {
+	calculateSimilarityScore,
+	findAllDistances,
+	findTotalDistance,
+	parseDataIntoSortedArrays,
+} from "./1";
+
+describe("Day 1", async () => {
+	const file = Bun.file(`${__dirname}/1.sample.txt`);
+	const rawText = await file.text();
+
+	const { left, right } = parseDataIntoSortedArrays(rawText);
+	const distances = findAllDistances(left, right);
+
+	describe("Part 1", () => {
+		describe("Extracts into arrays", () => {
+			test("Left array is correct", () => {
+				expect(left).toBeArray();
+				expect(left).toEqual([1, 2, 3, 3, 3, 4]);
+			});
+			test("Right array is correct", () => {
+				expect(right).toBeArray();
+				expect(right).toEqual([3, 3, 3, 4, 5, 9]);
+			});
+		});
+
+		describe("Calculates distances", () => {
+			test("Distances are correct", () => {
+				expect(distances).toEqual([2, 1, 0, 1, 2, 5]);
+			});
+
+			test("Distances accumulate correctly", () => {
+				expect(findTotalDistance(distances)).toEqual(11);
+			});
+		});
+	});
+
+	describe("Part 2", () => {
+		test("Similarity score is accurate", () => {
+			const similarityScore = calculateSimilarityScore(left, right);
+			expect(similarityScore).toEqual(31);
+		});
+	});
+});
---
+++ days/1.ts
@@ -0,0 +1,82 @@
+/*
+ * PART ONE
+ */
+
+export function parseDataIntoSortedArrays(data: string): {
+	left: number[];
+	right: number[];
+} {
+	const left: number[] = [];
+	const right: number[] = [];
+
+	for (const line of data.split(/[\r\n]/)) {
+		if (!line) continue;
+		const [leftNum, rightNum] = line.split("   ").map((n) => +n);
+		left.push(leftNum);
+		right.push(rightNum);
+	}
+
+	left.sort();
+	right.sort();
+
+	return { left, right };
+}
+
+export function findAllDistances(left: number[], right: number[]): number[] {
+	if ([left, right].some((array) => array.length !== left.length))
+		throw new Error(
+			"Some array passed to findAllDistances(arrays:) has mismatched length."
+		);
+
+	let distances: number[] = [];
+
+	for (let i = 0; i < left.length; i++) {
+		distances.push(left[i] - right[i]);
+	}
+
+	return distances.map(Math.abs);
+}
+
+export function findTotalDistance(distances: number[]): number {
+	return distances.reduce((prev, curr) => prev + curr, 0);
+}
+
+if (import.meta.main) {
+	const file = Bun.file(`${import.meta.dir}/1.input.txt`);
+	const rawText = await file.text();
+
+	const { left, right } = parseDataIntoSortedArrays(rawText);
+	const distances = findAllDistances(left, right);
+	const total = findTotalDistance(distances);
+
+	console.log(`Part 1 :: The total distance is: (solution) ${total}`);
+}
+
+/*
+ * PART TWO
+ */
+export function calculateSimilarityScore(
+	left: number[],
+	right: number[]
+): number {
+	let cumulativeScore = 0;
+
+	for (const leftNum of left) {
+		const appearancesInRightArray = right.filter(
+			(num) => num === leftNum
+		).length;
+		cumulativeScore += leftNum * appearancesInRightArray;
+	}
+
+	return cumulativeScore;
+}
+
+if (import.meta.main) {
+	const file = Bun.file(`${import.meta.dir}/1.input.txt`);
+	const rawText = await file.text();
+
+	const { left, right } = parseDataIntoSortedArrays(rawText);
+	const score = calculateSimilarityScore(left, right);
+
+	console.log(`Part 2 :: The similarity score is: (solution) ${score}`);
+}
